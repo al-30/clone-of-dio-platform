6c0fce2b7885add11d1268ed5985241c60796f29
Alfredo <gabriel94alfredo@gmail.com>
2025-05-23 17:46:39 -0300

    feat: create button component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import Button from '../../components/Button';
 
 export default function Home() {
   return (
     <>
       <div>Home</div>
-      <Link to='/login'>Fazer Login</Link>
+      <Button title={'teste 1'} />
+      <Button variant='secondary' title={'teste 2'} />
+      <Link to='/login'>Ir para o login</Link>
     </>
   );
 }
