7e327ba708e65b766dc626a5c940d5b83d4be817
Alfredo <gabriel94alfredo@gmail.com>
2025-05-23 23:24:50 -0300

    feat: create header components

---
+++ src/components/Header/styles.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 80%;
+  height: 47px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  algin-items: center;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  background-color: #151515;
+  width: 100%;
+  height: 47px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BuscarInputContainer = styled.div`
+  background: #2d2d37;
+  border-radius: 8px;
+  height: 30px;
+  margin: 0 12px;
+  padding: 2px 5px;
+  width: 175px;
+`;
+
+export const Menu = styled.a`
+  color: #ffffff;
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-size: 12px;
+  line-height: 25px;
+  margin-right: 12px;
+  text-decoration: none;
+`;
+export const MenuRight = styled.a`
+  color: #ffffff;
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-size: 12px;
+  line-height: 25px;
+  margin-right: 12px;
+  text-decoration: none;
+`;
+
+export const UserPicture = styled.img`
+  border-radius: 22px;
+  border: 2px solid #ffffff;
+  height: 32px;
+  width: 32px;
+`;
+
+export const Input = styled.input`
+  background: transparent;
+  border: 0;
+  color: #ffffff;
+  flex: 1;
+`;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
+import Header from '../../components/Header';
 
 export default function Home() {
   return (
     <>
-      <div>Home</div>
+      <Header />
       <Button title={'teste 1'} />
       <Button variant='secondary' title={'teste 2'} />
       <Link to='/login'>Ir para o login</Link>
