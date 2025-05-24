7cc89932256417809e15c821f2633bfa94489492
Alfredo <gabriel94alfredo@gmail.com>
2025-05-24 18:50:52 -0300

    feat: create feed page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
+import Feed from './pages/feed';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/feed' element={<Feed />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/feed/index.jsx
@@ -0,0 +1,14 @@
+import Card from '../../components/Card';
+import Header from '../../components/Header';
+import { Container } from './styles';
+
+export default function Feed() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Card />
+      </Container>
+    </>
+  );
+}
