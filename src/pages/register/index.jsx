82d9191a016957ad9ec1afbc079ea7d91eda933f
Alfredo <gabriel94alfredo@gmail.com>
2025-05-28 11:49:36 -0300

    feat: created registration page

---
+++ src/pages/register/index.jsx
@@ -0,0 +1,97 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { MdEmail, MdLock, MdPerson } from 'react-icons/md';
+import * as yup from 'yup';
+import Button from '../../components/Button';
+import Header from '../../components/Header';
+import Input from '../../components/Input';
+import {
+  Column,
+  Container,
+  SubtitleRegister,
+  Text,
+  TextLogin,
+  Title,
+  TitleRegister,
+  Wrapper,
+} from './styles';
+
+export default function Register() {
+  const schema = yup.object({
+    email: yup
+      .string()
+      .email('email não é valido')
+      .required('campo obrigatorio'),
+    name: yup.string().required('Campo obrigatorio'),
+    password: yup
+      .string()
+      .min(3, 'A senha dever ter no minimo 3 caracteres')
+      .required('Campo Obrigatorio'),
+  });
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema), mode: 'onChange' });
+
+  const onSubmit = async (formData) => {
+    console.log(`email: ${formData.email}; senha: ${formData.password}`);
+  };
+  return (
+    <>
+      <Header />
+      <Container>
+        <Column>
+          <Title>
+            A plataforma para você aprender com experts, dominar as principais
+            tecnologias e entrar mais rápido nas empresas mais desejadas.
+          </Title>
+        </Column>
+        <Column>
+          <TitleRegister>Comece agora grátis</TitleRegister>
+          <SubtitleRegister>
+            Crie sua conta e make the change._
+          </SubtitleRegister>
+
+          <Wrapper>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                placeholder='Nome Completo'
+                name='name'
+                control={control}
+                errorMessage={errors.name?.message}
+                leftIcon={<MdPerson />}
+              />
+              <Input
+                placeholder='E-mail'
+                name='email'
+                errorMessage={errors.email?.message}
+                control={control}
+                leftIcon={<MdEmail />}
+              />
+              <Input
+                placeholder='Password'
+                name='password'
+                errorMessage={errors.password?.message}
+                control={control}
+                leftIcon={<MdLock />}
+              />
+              <Button
+                title='Criar minha conta'
+                variant='secondary'
+                type='submit'
+              />
+            </form>
+            <Text>
+              Ao clicar em "criar minha conta grátis", declaro que aceito as
+              Políticas de Privacidade e os Termos de Uso da DIO.
+            </Text>
+            <TextLogin>
+              Já tenho conta. <span>Fazer Login</span>
+            </TextLogin>
+          </Wrapper>
+        </Column>
+      </Container>
+    </>
+  );
+}
---
+++ src/pages/register/styles.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 141px;
+  width: 100%;
+  height: 308px;
+`;
+
+export const Title = styled.h2`
+  font-weight: 700;
+  font-size: 32px;
+  height: 308px;
+  margin-left: 65px;
+  margin-top: 141px;
+  width: 388px;
+  font-family: 'Open Sans';
+`;
+export const Column = styled.div`
+  flex: 1;
+`;
+export const Wrapper = styled.div`
+  max-width: 280px;
+`;
+export const TitleRegister = styled.p`
+  font-family: 'Open Sans';
+  width: 368px;
+  height: 44px;
+  margin-top: 141px;
+  font-weight: 700;
+  font-size: 32px;
+`;
+export const SubtitleRegister = styled.p`
+  font-family: 'Open Sans';
+  margin-top: 10px;
+  margin-bottom: 30px;
+  font-weight: 400;
+  font-size: 18px;
+  width: 296px;
+  height: 25px;
+  line-height: 100%;
+`;
+export const Text = styled.p`
+  margin-top: 32px;
+  font-weight: 400;
+  font-size: 18px;
+  font-family: 'Open Sans';
+  width: 372px;
+`;
+export const TextLogin = styled.p`
+  margin-top: 20px;
+  font-weight: 700;
+  font-size: 14px;
+  font-family: 'Open Sans';
+
+  span {
+    color: #23dd7a;
+  }
+`;
